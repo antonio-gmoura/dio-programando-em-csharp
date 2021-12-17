a6da791a70231be5a6cd2c82e6418e9179a7943a
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-17 00:48:53 -0400

    Added Desafio 1

--- Desafio1/Program.cs
+++ Desafio1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            while(N-- > 0)
+            {
+                int val = Convert.ToInt32(Console.ReadLine());
+                int newval = (int)Math.Sqrt(val);
+                int resposta = val - newval;
+                Console.WriteLine(resposta);
+            }
+        }
+    }
+}
