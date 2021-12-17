979a5d52fae602c084ebad24b383eee810025f08
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-16 20:24:19 -0400

    Adicionar arquivos de projeto.

---
+++ Desafio2/Program.cs
@@ -0,0 +1,8 @@
+﻿        int n = int.Parse(Console.ReadLine());
+        long r;
+        for (int p = 2; p <= n; p++)
+        {
+            r = (p * p);
+            Console.WriteLine($"{p}^2 = {r}");
+            p++;
+        }
