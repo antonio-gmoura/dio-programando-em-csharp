8c1166fc8718e0601354fa13a7aa543aff7bdea4
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-16 23:35:59 -0400

    Adjusments

--- Desafio2/Program.cs
+++ Desafio2/Program.cs
@@ -1,8 +1,18 @@
-﻿        int n = int.Parse(Console.ReadLine());
-        long r;
-        for (int p = 2; p <= n; p++)
+﻿using System;
+
+namespace Desafio6
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            r = (p * p);
-            Console.WriteLine($"{p}^2 = {r}");
-            p++;
+            int n = int.Parse(Console.ReadLine());
+            long r;
+            for (int p = 2; p <= n; p+=2)
+            {
+                r = (p * p);
+                Console.WriteLine($"{p}^2 = {r}");
+            }
         }
+    }
+}
--- Desafio4/Program.cs
+++ Desafio4/Program.cs
@@ -6,13 +6,12 @@ namespace Desafio4
     {
         static void Main(string[] args)
         {
-            for(int i = 1; i <= 9; i++)
+            for(int i = 1; i <= 9; i+=2)
             {
                 for(int j = 7; j >= 5; j--) 
                 {
                     Console.WriteLine($"I={i} J={j}"); // I=1 J=7
-                }
-                i++;
+                };
             }
         }
     }
