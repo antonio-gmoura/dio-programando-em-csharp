39dc25e72d45259cda4e54312390f35f69a30ffd
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-17 00:53:33 -0400

    Adjusments

--- Desafio2/Program.cs
+++ Desafio2/Program.cs
@@ -6,7 +6,7 @@ namespace Desafio6
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
             long r;
             for (int p = 2; p <= n; p+=2)
             {
