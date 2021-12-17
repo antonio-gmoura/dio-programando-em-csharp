928081d29cb74bb1c2e75cb907e52a552f6916ad
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-16 22:05:16 -0400

    Added Desafio 4

---
+++ Desafio4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+                for(int j = 7; j >= 5; j--) 
+                {
+                    Console.WriteLine($"I={i} J={j}"); // I=1 J=7
+                }
+                i++;
+            }
+        }
+    }
+}
