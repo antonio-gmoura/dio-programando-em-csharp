3f406b32737db82d9eca5177dc3e1cb5213de8d8
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-16 21:56:17 -0400

    Desafio 3 solved

--- Desafio3/Program.cs
+++ Desafio3/Program.cs
@@ -6,7 +6,14 @@ namespace Desafio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, c, S = 0;
+            for (a = 1; a <= 100; a++)
+            {
+                c = 1 / a;  //coloque a sua lógica nos espaços em branco
+                S += c;
+            }
+            var x=Math.Round(S,2);
+            Console.WriteLine(x);
         }
     }
 }
