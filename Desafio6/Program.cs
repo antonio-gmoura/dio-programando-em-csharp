3bb52ff43b462efe930acfe2bf78eebd0b98a201
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-16 23:13:45 -0400

    Added Desafio 6

---
+++ Desafio6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Desafio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            x += (x % 2 == 0 ? 2 : 1);
+            Console.WriteLine(x);
+        }
+    }
+}
